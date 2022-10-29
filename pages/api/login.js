1fed576720f950eda29770fd07ce477f9bcd0d77
Syed Hussain Saber <98306229+Syedhussainsaber@users.noreply.github.com>
2022-10-29 18:44:26 +0530

    initial commit

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,25 +1,19 @@
 // pages/api/login.js
 
-import { Magic } from '@magic-sdk/admin'
-import Iron from '@hapi/iron'
-import CookieService from '../../lib/cookie'
+import { Magic } from "@magic-sdk/admin"
+import Iron from "@hapi/iron"
+import CookieService from "../../lib/cookie"
 
 export default async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).end()
+    if (req.method !== "POST") return res.status(405).end()
 
-  // exchange the did from Magic for some user data
-  const did = req.headers.authorization.split('Bearer').pop().trim()
-  const user = await new Magic(
-   "sk_live_F1B971842F8CAA4E",
-  ).users.getMetadataByToken(did)
+    // exchange the did from Magic for some user data
+    const did = req.headers.authorization.split("Bearer").pop().trim()
+    const user = await new Magic(process.env.MAGIC_SECRET_KEY).users.getMetadataByToken(did)
 
-  // Author a couple of cookies to persist a user's session
-  const token = await Iron.seal(
-    user,
-    "This is a secret magic key bitovia",
-    Iron.defaults,
-  )
-  CookieService.setTokenCookie(res, token)
+    // Author a couple of cookies to persist a user's session
+    const token = await Iron.seal(user, process.env.ENCRYPTION_SECRET, Iron.defaults)
+    CookieService.setTokenCookie(res, token)
 
-  res.end()
+    res.end()
 }
