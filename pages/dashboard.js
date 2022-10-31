ed6830047ba3c8813a825bae1d39b61334ba2821
Syed Hussain Saber <98306229+Syedhussainsaber@users.noreply.github.com>
2022-11-01 01:13:51 +0530

    initial commit

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,6 +7,15 @@ import Script from "next/script"
 import { ethers } from "ethers"
 import Moralis from "moralis"
 import { EvmChain } from "@moralisweb3/evm-utils"
+
+import {
+    contractAddressReferral,
+    contractAddressStaking,
+    contractAddressReward,
+    abiReward,
+    abiReferral,
+    abiStaking,
+} from "../constants"
 // import { abi } from "./abi"
 // import Bitovia from "../../contracts/Bitovia"
 // import { contractAddress, abi } from "../constants"
@@ -34,44 +43,74 @@ const dashboard = () => {
     // https://bsc-dataseed.binance.org/
 
     // const provider = new ethers.providers.Web3Provider()
-    const key = "0dbcfd7004b8415d4bb1f716161c44b58e07cc957c0724ab625415c5e3b88a48"
+    const key = process.env.PRIVATE_KEY
+
     // const hexPrivateKey = new Buffer.from(key, "hex")
     // const signer = new ethers.Wallet(hexPrivateKey, provider)
     // const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/")
-    const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/")
-    // const signers = provider.getSigner()
-    const tokenAddress = "0x1a1fC1Da5F603cAfe6fb000933ab3E289061fb29"
-    // const wallet = new ethers.Wallet(key, provider)
-    const signers = provider.getSigner()
+    // async function Staking(ethAmount) {
 
-    async function balanceOf() {
-        if (typeof window.ethereum != "undefined") {
-            console.log(provider)
+    async function stakeFun() {
+        if (typeof window.ethereum !== "undefined") {
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const signers = provider.getSigner()
+            const staking = await new ethers.Contract(contractAddressStaking, abiStaking, signers)
+            const plan_expired = await staking.planExpired()
+            const interestRate = await staking.interestRate()
+            console.log(plan_expired.toNumber())
+            console.log(interestRate)
+            const owner = await staking.owner()
+            console.log(owner)
+            console.log(staking)
+            const stakedAmount = document.getElementById("stakingAmount").value
 
-            const chain = EvmChain.BSC
-            // 0x1a1fC1Da5F603cAfe6fb000933ab3E289061fb29
-            const address = "0xc49c45546bc48f4fee8bd4c4093a08eb96c86773"
+            // const signers = provider.getSigner()
+            const TOKEN_ADDRESS = "0x1a1fC1Da5F603cAfe6fb000933ab3E289061fb29"
 
-            await Moralis.start({
-                apiKey: "wbI5oqzeRgWLehcxiJZB3NuTUTqJux1t0aObDams1OP1WAiiZSs4dRE5lbgQ2yo2",
-                // ...and any other configuration
-            })
+            const tokenAbi = [
+                "function approve(address spender, uint256 amount) public override returns (bool)",
+            ]
+            const rewardToken = await new ethers.Contract(TOKEN_ADDRESS, tokenAbi, signers)
+            const balance = await provider.getBalance(TOKEN_ADDRESS)
+            console.log(ethers.utils.formatUnits(balance, 18))
 
-            const response = await Moralis.EvmApi.token.getWalletTokenBalances({
-                address,
-                chain,
-            })
-            console.log(response.data[2])
-            const balance = ethers.utils.formatUnits(response.data[2].balance, 18)
+            const response = await rewardToken.approve(
+                contractAddressStaking,
+                ethers.utils.parseUnits(stakedAmount)
+            )
+            console.log(response)
 
-            document.querySelector(".balance").innerText = `${balance}`
-            // console.log(provider.getBlockNumber())
-        } else {
-            document.write("Install the Metamask")
+            // const abiToken = [
+            //     "function approve(address spender, uint256 amount) public returns (bool)",
+            // ]
+            // const TOKEN_ADDRESS = "0x1a1fC1Da5F603cAfe6fb000933ab3E289061fb29"
+            // const tokenContract = await new ethers.Contract(TOKEN_ADDRESS, abiStaking, signers)
+
+            // const approveToken = await tokenContract.approve(
+            //     contractAddressStaking,
+            //     1000000000000000000
+            // )
+            // console.log(approveToken)
+
+            const stakeTokens = await staking.stakeToken(ethers.utils.parseEther(stakedAmount))
+            const transactionRecipt = await stakeTokens.wait(1)
+            document.querySelector(".stakeAmount").innerText = stakedAmount
+            console.log(transactionRecipt)
+
+            // const wallet = new ethers.Wallet(key, provider)
         }
     }
 
-    async function send() {}
+    async function claimFun() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signers = provider.getSigner()
+        const staking = await new ethers.Contract(contractAddressStaking, abiStaking, signers)
+
+        const claimToken = await staking.claimReward()
+        const transactionRecipt = await claimToken.wait(1)
+        console.log(transactionRecipt)
+        stakedAmount = stakedAmount - 0.0016 * stakedAmount
+    }
 
     return (
         <>
@@ -95,15 +134,44 @@ const dashboard = () => {
             <div className="containerDashboard">
                 {/* {console.log(ethers)} */}
                 <Dashboard />
-                <button className="btn btn-success" onClick={balanceOf}>
-                    Balance
-                </button>
-                <p className="balance"></p>
-                {/* <input type="text" id="amount" /> */}
-                {/* <input type="text" id="receiver" placeholder="Enter the address" /> */}
-                <button className="btn btn-primary" onClick={send}>
-                    Send
-                </button>
+                {/* <p className="text-white mx-3 mt-2 text-align-right">
+                    {loading ? "loading" : user.email}
+                </p> */}
+                <h2
+                    className="text-white text-center"
+                    style={{ marginTop: "20px", fontSize: "2.2em" }}
+                >
+                    Stake And Earn
+                </h2>
+                <div className="stakeContainer">
+                    <p className="stakePlan"> 3 months Plan</p>
+                    <img
+                        src="/Bitovia.png"
+                        alt="Bitovia"
+                        style={{ width: "52px", margin: "15px" }}
+                    />
+                    <p className="stakeAmount"> {"0 BTV"}</p>
+                    <div className="stakeBtns">
+                        <div className="stakeFeature">
+                            <input
+                                type="text"
+                                placeholder="0 BTV"
+                                name="stakeAmount"
+                                id="stakingAmount"
+                            />
+
+                            <button className="btn btn-warning claimBtn" onClick={claimFun}>
+                                Claim
+                            </button>
+                        </div>
+                        <button className="btn btn-primary stakeBtn" onClick={stakeFun}>
+                            Stake
+                        </button>
+                    </div>
+                </div>
+                <p style={{ fontSize: "1.3em" }} className="text-center">
+                    Your Airdrop Reward : 1000000BTV
+                </p>
             </div>
         </>
     )
